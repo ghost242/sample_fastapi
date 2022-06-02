522a6ac97bf4cfc76f919e50ec5218986ed8514a
jeffrey <aroh@madup.com>
2022-06-02 16:27:53 +0900

    Test code more enhanced. add `.gitignore`. add README.md.

---
+++ README.md
@@ -0,0 +1,12 @@
+[![Build Status](https://app.travis-ci.com/ghost242/sample_fastapi.svg?branch=master)](https://app.travis-ci.com/ghost242/sample_fastapi)
+
+# Deploy Test
+
+Test project of deployment with TravisCI and docker.
+
+## Purpose
+
+This project is for test about how do I test on TravisCI with PostgreSQL on Docker. It is based on framework, 
+FastAPI and SQLAlchemy. Especially, It will connect PostgreSQL from SQLAlchemy. 
+
+PostgreSQL Database server set on Docker.
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,21 @@
-from fastapi.testclient import TestClient
+from uuid import uuid4
+
+from src.models.database import SessionInst
+from src.models.schemas import DevModel
 from src import service_app
 
+from fastapi.testclient import TestClient
+from pytest import fixture
+
 client = TestClient(service_app)
+
+@fixture()
+def model_data():
+    return {"key": str(uuid4()), "value": "World!"}
+
+@fixture()
+def session():
+    return SessionInst()
 
 def test_app_index():
     response = client.get("/")
@@ -9,11 +23,21 @@ def test_app_index():
     assert response.json() == {"message": "Hello world!!"}
 
 
-def test_insert():
-    response_post = client.post("/insert", json={"key": "Hello", "value": "World!"})
+def test_insert(session, model_data):
+    response_post = client.post("/insert", json=model_data)
 
-    assert response_post.json() == {"key": "Hello", "value": "World!"}
+    assert response_post.json() == model_data
 
-    response_get = client.get("/Hello")
+    records = session.query(DevModel).filter(DevModel.key==model_data['key']).all()
 
-    assert response_get.json() == {"key": "Hello", "value": "World!"}
+    assert len(records) > 0
+
+def test_get(session, model_data):
+    record = DevModel(key=str(uuid4()), value="This is value column.")
+    session.add(record)
+    session.commit()
+
+    response_get = client.get(f"/{record.key}").json()
+
+    assert response_get["key"] == record.key
+    assert response_get["value"] == record.value
