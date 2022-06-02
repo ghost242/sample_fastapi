38b721c904f73f651b628be09081e1f5d6e5fedf
jeffrey <aroh@madup.com>
2022-06-02 11:24:13 +0900

    add test code

--- src/client/crud.py
+++ src/client/crud.py
@@ -1,18 +1,23 @@
 from sqlalchemy.orm import Session
 
-from src.models import InsertDevModel
+from src.models import GetDevModel, InsertDevModel
 from src.models.schemas import DevModel
 
 
 def get_dev_model(db: Session, key: str):
-    return db.query(DevModel).filter(DevModel.key == key).one()
+    record = db.query(DevModel).filter(DevModel.key == key).first()
+
+    return GetDevModel(key=record.key, value=record.value)
+
 
 def create_dev_model(db: Session, model: InsertDevModel):
     record = DevModel(key=model.key, value=model.value)
 
-    res = db.add(record)
-
-    if res:
-        return record
+    try:
+        db.add(record)
+    except:
+        db.rollback()
     else:
-        return None
+        db.commit()
+
+        return record
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,24 @@
 from fastapi.testclient import TestClient
 
+from app import service_initializer
 from src import service_app
 
 client = TestClient(service_app)
+
+def pytest_sessionstart():
+    service_initializer()
 
 def test_app_index():
     response = client.get("/")
 
     assert response.json() == {"message": "Hello world!!"}
+
+
+def test_insert():
+    response_post = client.post("/insert", json={"key": "Hello", "value": "World!"})
+
+    assert response_post.json() == {"key": "Hello", "value": "World!"}
+
+    response_get = client.get("/Hello")
+
+    assert response_get.json() == {"key": "Hello", "value": "World!"}
